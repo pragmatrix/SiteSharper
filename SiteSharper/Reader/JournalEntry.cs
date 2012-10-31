38b0601728d1b6d72ed2b6edeac497223099251d
Armin Sander <armin@replicator.org>
2012-10-31 19:07:46 +0100

    add pubDate and guid RSS elements

--- SiteSharper/Reader/JournalEntry.cs
+++ SiteSharper/Reader/JournalEntry.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.Text;
 
 namespace SiteSharper.Reader
 {
@@ -8,6 +10,21 @@ namespace SiteSharper.Reader
 		public JournalEntryFilename Filename;
 		public string Content;
 		public DateTime Date;
+
+		// http://stackoverflow.com/questions/284775/how-do-i-parse-and-convert-datetimes-to-the-rfc-822-date-time-format
+
+		public string RFC822Date
+		{
+			get
+			{
+				var dto = new DateTimeOffset(Date);
+				if (dto.Offset == TimeSpan.Zero)
+					return dto.ToUniversalTime().ToString("ddd, dd MMM yyyy HH:mm:ss Z", CultureInfo.InvariantCulture);
+				var builder = new StringBuilder(dto.ToString("ddd, dd MMM yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture));
+				builder.Remove(builder.Length - 3, 1);
+				return builder.ToString();
+			}
+		}
 
 		public string Title
 		{
